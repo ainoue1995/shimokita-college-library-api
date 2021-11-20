7f8d7816c249f9607a939ede037732e38d8f4620
ainoue1995 <inoueakio0205@gmail.com>
2021-11-20 15:42:19 +0900

    change port to use env variables

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Start on port 3000.')
 })
 
